ea37ef72bb073044a38886c76a383f0165cea375
Yy77501-sin <yasenyemeni00@gmail.com>
2026-09-01 07:15:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 # -------------------------------------------------------------
 
 # توكن البوت الجديد الآمن
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8998307482:AAHlUFDu0E_0ltdVQVEGQ5z2pats24kK3rU")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8717009668:AAE07ahqxrBFpgto2RJ0R8Kha9WEpofxS74")
 
 # مفتاح مزود الأرقام (Hero SMS)
 HERO_SMS_API_KEY = os.getenv("HERO_SMS_API_KEY", "b3040375cf393b48dfce23793c4efb2d")
